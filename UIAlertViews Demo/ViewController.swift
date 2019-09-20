cd69dee73ad918de839e17f3306e888e49074bc4
Mirko Cukich <mirkocukich@gmail.com>
2019-09-19 23:24:01 -0500

    Program Complete

--- UIAlertViews Demo/ViewController.swift
+++ UIAlertViews Demo/ViewController.swift
@@ -21,10 +21,30 @@ class ViewController: UIViewController {
 
     @IBAction func showAlertBtn(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Unable to log in", message: "The email or password that you\ntyped in is incorrect", preferredStyle: .alert)
         
+        let actionButton = UIAlertAction(title: "Action", style: .default , handler: {
+            
+            (alert: UIAlertAction!) -> Void in
+            self.alertMessageLbl.isHidden = false
+            self.alertMessageLbl.text = "Action Requested"
+
+        })
         
+        let dismissButton = UIAlertAction(title: "Dismiss", style: .cancel, handler: {
+            
+            (alert: UIAlertAction!) -> Void in
+            self.alertMessageLbl.isHidden = false
+            self.alertMessageLbl.text = "Alert Acknowledged"
+
+        })
+        
+        alert.addAction(actionButton)
+        alert.addAction(dismissButton)
+        
+        // present alert to user
+        self.present(alert, animated: true, completion: nil)
         
     }
     
 }
-
